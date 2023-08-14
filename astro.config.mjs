7c89bb90f3b87bead1a9de79888d58132b175635
Noxitb <splitgaming101yt@gmail.com>
2023-08-15 08:57:40 +1000

    Main

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,10 @@
-import { defineConfig } from 'astro/config'
-import svelte from '@astrojs/svelte'
-import mdx from '@astrojs/mdx'
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astro-blog-template.netlify.app',
-  integrations: [mdx(), svelte()],
-  markdown: {
-    shikiConfig: {
-      theme: 'nord',
-    },
-    remarkPlugins: ['remark-gfm', 'remark-smartypants'],
-    rehypePlugins: [
-      [
-        'rehype-external-links',
-        {
-          target: '_blank',
-        },
-      ],
-    ],
-  },
-})
+	site: 'https://example.com',
+	integrations: [mdx(), sitemap()],
+});
