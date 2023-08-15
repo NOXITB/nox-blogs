ea07d6e0b83c645206cd9695878e49ea92b82b4e
Noxitb <splitgaming101yt@gmail.com>
2023-08-15 12:28:58 +1000

    up

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Blakes Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_DESCRIPTION =
+  "Welcome to my website where I do blogs on coding servers and tech.";
