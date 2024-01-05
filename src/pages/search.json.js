75b2877aa94fb25de15c2e25aa5825cf3f3c1e02
Noxitb <splitgaming101yt@gmail.com>
2024-01-06 00:56:37 +1000

    Update Search

--- src/pages/search.json.js
+++ src/pages/search.json.js
@@ -1,29 +1,24 @@
 import { getCollection } from "astro:content";
 
-export const latestPosts = (
-  await getCollection("blog", ({ data }) => {
-    return data.draft !== true;
-  })
-).sort(
-  (a, b) =>
-    new Date(b.data.publishDate).valueOf() -
-    new Date(a.data.publishDate).valueOf()
-);
+export const latestPosts = await getCollection("blog", ({ data }) => {
+  return data.draft !== true;
+});
 
 async function getPosts() {
   // Use the latestPosts array to avoid fetching the collection again
-  const posts = latestPosts;
-  return posts.map((post) => ({
+  return latestPosts.map((post) => ({
     slug: post.slug,
     title: post.data.title,
     description: post.data.excerpt,
     date: post.data.publishDate,
-    category: post.data.category 
+    category: post.data.category,
+    tags: post.data.tags,
   }));
 }
 
 export async function get() {
-  return new Response(JSON.stringify(await getPosts()), {
+  const posts = await getPosts();
+  return new Response(JSON.stringify(posts), {
     status: 200,
     headers: {
       "Content-Type": "application/json",
